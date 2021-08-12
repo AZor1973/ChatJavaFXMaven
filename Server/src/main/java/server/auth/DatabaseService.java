a0520319b5948575f4afd6feacde4d2b9cc803a1
AZor1973 <azor@mail.ru>
2021-08-12 12:02:57 +0300

    lesson2.2

--- Server/src/main/java/server/auth/DatabaseService.java
+++ Server/src/main/java/server/auth/DatabaseService.java
@@ -3,26 +3,29 @@ package server.auth;
 import java.sql.*;
 
 public class DatabaseService {
+    public static final String GET_USERNAME_REQUEST = "SELECT username FROM users WHERE login = ? AND password = ?";
     private static final String DB_URL = "jdbc:sqlite:E:\\java\\ChatJavaFXMaven\\chat.db";
+    public static final String CHANGE_USERNAME_REQUEST = "UPDATE users SET username = ? WHERE login = ? AND password = ?";
     private Connection connection;
+    private PreparedStatement getUsernameStatement;
+    private PreparedStatement changeUsernameStatement;
 
     public DatabaseService() {
         try {
             connection = DriverManager.getConnection(DB_URL);
+            getUsernameStatement = connection.prepareStatement(GET_USERNAME_REQUEST);
+            changeUsernameStatement = connection.prepareStatement(CHANGE_USERNAME_REQUEST);
         } catch (SQLException e) {
             System.out.println("Failed to database connection");
         }
     }
 
-
     public String getUsernameByLoginAndPassword(String login, String password) {
         String username = null;
-        String request = "SELECT username FROM users WHERE login = ? AND password = ?";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(request);
-            preparedStatement.setString(1, login);
-            preparedStatement.setString(2, password);
-            ResultSet resultSet = preparedStatement.executeQuery();
+            getUsernameStatement.setString(1, login);
+            getUsernameStatement.setString(2, password);
+            ResultSet resultSet = getUsernameStatement.executeQuery();
             while (resultSet.next()) {
                 username = resultSet.getString("username");
             }
@@ -33,13 +36,11 @@ public class DatabaseService {
     }
 
     public void changeUsername(String newUsername, String login, String password) {
-        String request = "UPDATE users SET username = ? WHERE login = ? AND password = ?";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(request);
-            preparedStatement.setString(1, newUsername);
-            preparedStatement.setString(2, login);
-            preparedStatement.setString(3, password);
-            preparedStatement.executeUpdate();
+            changeUsernameStatement.setString(1, newUsername);
+            changeUsernameStatement.setString(2, login);
+            changeUsernameStatement.setString(3, password);
+            changeUsernameStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Failed to database connection");
         }
