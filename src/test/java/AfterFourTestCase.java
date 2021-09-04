e90302a819256b5550806153574f6030e4643095
AZor1973 <azor@mail.ru>
2021-09-04 11:18:56 +0300

    lesson6.1

--- src/test/java/AfterFourTestCase.java
+++ src/test/java/AfterFourTestCase.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestAfterFour {
+public class AfterFourTestCase {
     private Lesson6 less;
 
     @BeforeEach
